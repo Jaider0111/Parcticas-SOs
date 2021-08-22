6fb11f06a75a2da89c58bbdf499649c0bb7dbcc1
este6an13 <57577966+este6an13@users.noreply.github.com>
2021-08-22 16:34:47 -0500

    Create mfifoc.c

---
+++ Taller/mfifoc.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <time.h>
+#include <math.h>
+#include <unistd.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#define PERMISSIONS 0666
+
+int main() {
+
+	char *pipea = "/tmp/pipea";	
+	unlink(pipea);
+	int r = mkfifo(pipea, PERMISSIONS);
+	if (r < 0) {
+    printf("error creating pipea\n");
+		exit(-1);
+	}
+	char *pipeb = "/tmp/pipeb";
+	unlink(pipeb);
+	r = mkfifo(pipeb, PERMISSIONS);
+	if (r < 0) {
+    printf("error creating pipeb\n");
+		exit(-1);
+	}
+	char *pipec = "/tmp/pipec";
+	unlink(pipec);
+	r = mkfifo(pipec, PERMISSIONS);
+	if (r < 0) {
+    printf("error creating pipec\n");
+		exit(-1);
+	}
+	int pr, pw, psr;
+	char signal[2];
+	int KB = 1024;
+	int sizes[] = { KB, 10*KB, 100*KB, KB*KB, 10*KB*KB, 100*KB*KB };
+	for (int s = 0; s < 6; s++) {
+		int bytes = sizes[s];
+		char *data = (char *) malloc(bytes * sizeof(char));
+		int reps = (bytes <= KB*KB) ? 20 : 8;
+		pr = open(pipea, O_RDONLY);	
+		for (int i = 0; i < reps; i++) {
+			psr = open(pipec, O_RDONLY);
+			int r = read(psr, signal, 2);
+			close(psr);
+			int len;
+			int received = 0;
+			while ((len = read(pr, data, bytes)) > 0) {
+				received += len;
+			}
+			printf("Received %d\n", received);
+			pw = open(pipeb, O_WRONLY);
+			write(pw, "r", 1);
+			close(pw);
+		}
+		close(pr);
+		free(data);
+	}
+	unlink(pipea);
+	unlink(pipeb);
+	unlink(pipec);
+	return 0;
+}

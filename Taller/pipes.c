a2939910bd30810747afc942a781177aa7ee71af
este6an13 <57577966+este6an13@users.noreply.github.com>
2021-08-23 21:45:22 -0500

    Update pipes.c

--- Taller/pipes.c
+++ Taller/pipes.c
@@ -17,29 +17,27 @@ int main() {
   int fdb[2];
   pipe(fda);
   pipe(fdb);
-  int response[2];
+  char response[2];
   int KB = 1024;
   int sizes[] = { KB, 10*KB, 100*KB, KB*KB, 10*KB*KB, 100*KB*KB };
   
-  if (fork() == 0) {
+  if (fork() > 0) {
     
     close(fda[READ]);
     close(fdb[WRITE]);
     for (int s = 0; s < 6; s++) {
     	int bytes = sizes[s];
     char *data = (char *) malloc(bytes * sizeof(char));
-	  for (int b = 0; b < bytes; b++)
-	    data[b] = 'a';
+    for (int b = 0; b < bytes; b++)
+        data[b] = 'a';
     struct timespec begin, end;
     int reps = (bytes <= KB*KB) ? 20 : 8;
-	  double total_time;
-	  long int secs, nsecs;
+    double total_time;
+    long int secs, nsecs;
     clock_gettime(CLOCK_REALTIME, &begin);
     for (int i = 0; i < reps; i++)
       write(fda[WRITE], data, bytes);
-    close(fda[WRITE]);
     read(fdb[READ], response, 2);
-    close(fdb[READ]);
     clock_gettime(CLOCK_REALTIME, &end);
     secs = end.tv_sec - begin.tv_sec;
     nsecs = end.tv_nsec - begin.tv_nsec;
@@ -50,24 +48,28 @@ int main() {
     else
         printf("El tiempo promedio para transferir %3dMB es: %.10f segundos\n", bytes/(KB*KB), avg);
     free(data);		
-      
+    }
+    close(fda[WRITE]);
+    close(fdb[READ]);
   } else {
+      close(fda[WRITE]);
+      close(fdb[READ]);
       for (int s = 0; s < 6; s++) {
         int bytes = sizes[s];
         char *data = (char *) malloc(bytes * sizeof(char));
         int reps = (bytes <= KB*KB) ? 20 : 8;
-        close(fda[WRITE]);
-        close(fdb[READ]);
+
         int len;
         int received = 0;
-        while ((len = read(fda[READ], data, bytes)) > 0) {
-            received += len;
-        }
+	for (int i = 0; i < reps; i++) {
+	   received += read(fda[READ], data, bytes);
+	}
         printf("Received %d\n", received);
         write(fdb[WRITE], "r", 1);
-        close(fda[READ]);
-        close(fdb[WRITE]);
         free(data);
+      }
+      close(fda[READ]);
+      close(fdb[WRITE]);
   }
   return 0;
 }
